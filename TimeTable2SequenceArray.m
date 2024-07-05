79bffcd73c7c2eee9bfe13c11456c15c8733858e
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-07-04 22:03:50 -0600

    generating pf, power and voltage sequence arrays

--- TimeTable2SequenceArray.m
+++ TimeTable2SequenceArray.m
@@ -1,13 +1,19 @@
-function [outputArray, outputLabelsArray] = TimeTable2SequenceArray(inputTimeTable, sample_size)
+function [currentArray, pfArray, powerArray, voltageArray, outputLabelsArray] = TimeTable2SequenceArray(inputTimeTable, sample_size)
 %jointApplianceInador suma los parametros de dos electrodomésticos
 %y recalcula los valores aparentes y factor de potencia.
 %   
-outputArray = [];
+currentArray = [];
+pfArray = [];
+powerArray = [];
+voltageArray = [];
 outputLabelsArray = [];
 [rows, ~] = size(inputTimeTable);
 iteration = 1;
 while sample_size * iteration <= rows
-    outputArray = [outputArray; inputTimeTable.current(((iteration-1)*sample_size)+1:iteration*sample_size)'];
+    currentArray = [currentArray; inputTimeTable.current(((iteration-1)*sample_size)+1:iteration*sample_size)'];
+    pfArray = [pfArray; inputTimeTable.pf(((iteration-1)*sample_size)+1:iteration*sample_size)'];
+    powerArray = [powerArray; inputTimeTable.power(((iteration-1)*sample_size)+1:iteration*sample_size)'];
+    voltageArray = [voltageArray; inputTimeTable.voltage(((iteration-1)*sample_size)+1:iteration*sample_size)'];
     outputLabelsArray = [outputLabelsArray; categorical(inputTimeTable.appliances(((iteration-1)*sample_size)+1))];
     iteration = iteration + 1;
 end
